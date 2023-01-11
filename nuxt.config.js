8f571f559cea23e2549148fb1fa7999b34c92481
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-01-11 14:03:30 -0600

    Add basic metatags for SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,37 @@
 import generate from './static-site-generation'
 
+const seoBase = {
+  title: 'Christopher Luna Mega | Composer',
+  description: 'Music Composer',
+  image: {
+    url: '/seo.jpeg',
+    width: 1024,
+    height: 768,
+  },
+}
+
+const openGraphData = {
+  url: 'https://christopherlunamega.com',
+  title: seoBase.title,
+  description: seoBase.description,
+  image: {
+    url: seoBase.image.url,
+    width: seoBase.image.width,
+    height: seoBase.image.height,
+  },
+}
+
+const makeOpenGraphMeta = (openGraphData) => [
+  { name: 'description', content: openGraphData.description },
+  { property: 'og:url', content: openGraphData.url },
+  { property: 'og:type', content: 'article' },
+  { property: 'og:title', content: openGraphData.title },
+  { property: 'og:description', content: openGraphData.description },
+  { property: 'og:image', content: openGraphData.image.url },
+  { property: 'og:image:width', content: openGraphData.image.width },
+  { property: 'og:image:height', content: openGraphData.image.height },
+]
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
@@ -14,9 +46,16 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        name: 'viewport',
+        content:
+          'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
+      },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      { name: 'application-name', content: 'Christopher Luna Mega' },
+      { name: 'twitter:card', content: 'summary' },
+      ...makeOpenGraphMeta(openGraphData),
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
