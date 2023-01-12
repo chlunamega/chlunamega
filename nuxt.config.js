d4fa23ea78312a47b2b640f02b4a80a40321ac42
Diego Villaseñor <diego.vid.eco@gmail.com>
2023-01-12 17:37:32 -0600

    Remove default favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -25,7 +25,9 @@ export default {
       { name: 'application-name', content: 'Christopher Luna Mega' },
       ...makeOpenGraphMeta(openGraphData),
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [
+      //  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
